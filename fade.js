fd6da02d989bf899e822bb319edc3bb20e8a6a0c
Eden <63891593+eden-garden@users.noreply.github.com>
2022-07-06 18:42:33 +0900

    ;

--- fade.js
+++ fade.js
@@ -4,16 +4,17 @@ function reveal() {
   for (var i = 0; i < reveals.length; i++) {
     var windowHeight = window.innerHeight;
     var elementTop = reveals[i].getBoundingClientRect().top;
-    var elementVisible = 100;
+    var elementVisible = 50;
 
     if (elementTop < windowHeight - elementVisible) {
       reveals[i].classList.add("active");
     } 
+    else {
+      reveals[i].classList.remove("active");
+    } /*스크롤을 다시 올렸을 때 사라짐*/
   }
 }
-// else {
-//   reveals[i].classList.remove("active");
-// } /*스크롤을 다시 올렸을 때 사라짐*/
+
 window.addEventListener("scroll", reveal);
 /* 
 채팅방
